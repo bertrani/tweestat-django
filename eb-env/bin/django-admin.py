c7c9b0d25c89a9d3541a7510c931fa31a47bdb56
Niels Bertram <bertramn@hu-berlin.de>
2018-08-30 13:23:20 +0200

    add gitignore and add virtualenv

---
+++ eb-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/niels/Workspace/TwitterStatistics/Tweestat/tweestat-django/eb-env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
